74e119c73d34654d1feccd8837ca16812f1bc39f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-03-29 10:27:43 +0900

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,35 @@
 {
   description = "A Fukuoka COVID-19 stats viewer in Rust/Dioxus";
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs;
-    flake-utils.url = github:numtide/flake-utils;
-  };
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage =
-        with import nixpkgs { system = "${system}"; };
-        stdenv.mkDerivation rec {
-          name = "fukuoka-c19-${version}";
-          pname = "fukuoka-c19";
-          version = "0.3.3-20220327";
-          src = self;
-          buildInputs = rustc.buildInputs ++ [ cargo rustc ] ++
-                        lib.optionals stdenv.isDarwin (
-                          with darwin.apple_sdk.frameworks; [ AppKit Carbon WebKit ]);
-          buildPhase = "cargo build --release";
-          installPhase = ''
-              mkdir -p $out/bin;
-              cp target/release/fukuoka-c19 $out/bin/fukuoka-c19
-          '';
-        }
-      ;
-    })
-  ;
+  {
+    packages = builtins.listToAttrs
+      (map
+        (system:
+          with import nixpkgs { system = "${system}"; };
+          {
+            name = system;
+            value = {
+              default =
+                stdenv.mkDerivation rec {
+                  name = "fukuoka-c19-${version}";
+                  pname = "fukuoka-c19";
+                  version = "0.3.3-20220327";
+                  src = self;
+                  buildInputs = rustc.buildInputs ++ [ cargo rustc ] ++
+                                lib.optionals stdenv.isDarwin (
+                                  with darwin.apple_sdk.frameworks; [ AppKit Carbon WebKit ]);
+                  buildPhase = "cargo build --release";
+                  installPhase = ''
+                      mkdir -p $out/bin;
+                      cp target/release/fukuoka-c19 $out/bin/fukuoka-c19
+                  '';
+                }
+              ;
+          };}
+        )
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+      )
+    ;
+  };
 }
